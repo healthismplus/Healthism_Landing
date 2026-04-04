ad8c1ad9e0079f40b37480d1cd0af088aba7b66f
y-ashbhatt <0yashbhatt0@gmail.com>
2026-04-04 13:42:16 +0530

    Implement Privacy Policy page layout and navigation functionality

--- frontend/src/pages/PrivacyPolicy.jsx
+++ frontend/src/pages/PrivacyPolicy.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import Footer from "@/components/shared/Footer";
+import Navigation from "@/components/shared/Navigation";
+import React, { useState } from "react";
 
 const PrivacyPolicy = () => {
-  return <div>PrivacyPolicy</div>;
+  const [activeTab, setActiveTab] = useState("individual");
+
+  const scrollToSection = (sectionId) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  return (
+    <div>
+      <Navigation
+        onSectionClick={scrollToSection}
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+      <section className="bg-[#EDF3F4] py-20 px-6 lg:px-12">
+        <div className="max-w-4xl mx-auto">
+          {/* HEADER */}
+          <h1 className="text-[32px] font-bold text-[#0F172A] mb-4">
+            PRIVACY POLICY
+          </h1>
+        </div>
+      </section>
+      <Footer onSectionClick={scrollToSection} setActiveTab={setActiveTab} />
+    </div>
+  );
 };
 
 export default PrivacyPolicy;
